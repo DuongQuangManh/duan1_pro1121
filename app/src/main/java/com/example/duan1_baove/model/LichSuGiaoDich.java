45a881a8a20ea62c92afcc4cea3f30fab9892de5
DuAn1 <manhdqph20768@fpt.edt.vn>
2022-11-15 14:28:44 +0700

    manhdqph20768_update database

--- app/src/main/java/com/example/duan1_baove/model/LichSuGiaoDich.java
+++ app/src/main/java/com/example/duan1_baove/model/LichSuGiaoDich.java
@@ -1,18 +1,27 @@
 package com.example.duan1_baove.model;
 
+import static androidx.room.ForeignKey.CASCADE;
 
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "lichsugiaodich",foreignKeys = {
+        @ForeignKey(entity = KhachHang.class,parentColumns = "soDienThoai",childColumns = "khachang_id",onDelete = CASCADE,onUpdate = CASCADE),
+        @ForeignKey(entity = DonHangChiTiet.class,parentColumns = "id",childColumns = "donhangchitiet_id",onDelete = CASCADE,onUpdate = CASCADE)
+})
 public class LichSuGiaoDich {
-
+    @PrimaryKey
     private int id;
-    private String KhachHang_id;
-    private String DonHangChiTiet_id;
+    private String khachang_id;
+    private int donhangchitiet_id;
     private String type;
     private int soTien;
 
-    public LichSuGiaoDich(int id, String khachHang_id, String donHangChiTiet_id, String type, int soTien) {
+    public LichSuGiaoDich(int id, String khachang_id, int donhangchitiet_id, String type, int soTien) {
         this.id = id;
-        this.KhachHang_id = khachHang_id;
-        this.DonHangChiTiet_id = donHangChiTiet_id;
+        this.khachang_id = khachang_id;
+        this.donhangchitiet_id = donhangchitiet_id;
         this.type = type;
         this.soTien = soTien;
     }
@@ -25,20 +34,20 @@ public class LichSuGiaoDich {
         this.id = id;
     }
 
-    public String getKhachHang_id() {
-        return KhachHang_id;
+    public String getKhachang_id() {
+        return khachang_id;
     }
 
-    public void setKhachHang_id(String khachHang_id) {
-        this.KhachHang_id = khachHang_id;
+    public void setKhachang_id(String khachang_id) {
+        this.khachang_id = khachang_id;
     }
 
-    public String getDonHangChiTiet_id() {
-        return DonHangChiTiet_id;
+    public int getDonhangchitiet_id() {
+        return donhangchitiet_id;
     }
 
-    public void setDonHangChiTiet_id(String donHangChiTiet_id) {
-        this.DonHangChiTiet_id = donHangChiTiet_id;
+    public void setDonhangchitiet_id(int donhangchitiet_id) {
+        this.donhangchitiet_id = donhangchitiet_id;
     }
 
     public String getType() {
