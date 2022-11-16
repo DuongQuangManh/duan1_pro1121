e8ec7545c31ca40f7f5c0caa2a88d0f24a77c211
viet <vietdhph26437@fpt.edu.vn>
2022-11-16 16:48:11 +0700

    vietdhph26437_fontend và database chức năng đăng kí tập thử

---
+++ app/src/main/java/com/example/duan1_baove/fragment/hocvien/DangKiTapThu_MainActivity_HocVien.java
@@ -0,0 +1,16 @@
+package com.example.duan1_baove.fragment.hocvien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.duan1_baove.R;
+
+public class DangKiTapThu_MainActivity_HocVien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ki_tap_thu_main_hoc_vien);
+    }
+}
